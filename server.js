503a67284747cc0a49fe740bbc431c4e38742773
Lisheng <lishengyu1986@gmail.com>
2020-05-06 00:06:03 +0800

    major update on server.js, all feature working on local

--- server.js
+++ server.js
@@ -1,49 +1,99 @@
-//create express server
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+let notes = require("./db/db.json");
 
+//create express server
 const app = express();
 const PORT = 3000;
 
-const notes = require("./db/db.json");
+const OUTPUT_DIR = path.resolve(__dirname, "db");
+const outputPath = path.join(OUTPUT_DIR, "db.json");
 
-//middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+function htmlRoute() {
+  // routing return index.html
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"));
+  });
 
-console.log("welcome");
+  // routing return notes.html
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./notes.html"));
+  });
+}
 
-// html router GET request handle
-app.get("/", function (req, res) {
-  console.log("default page loaded");
-  res.sendFile(path.join(__dirname, "./index.html"));
-});
+function apiRoute() {
+  // read db.json and return all saved notes
+  app.get("/api/notes", function (req, res) {
+    return res.json(notes);
+  });
 
-app.get("/notes", function (req, res) {
-  console.log("notes.html loaded");
-  res.sendFile(path.join(__dirname, "./notes.html"));
-});
+  // return single selected note
+  app.get("/api/notes/:id", function (req, res) {
+    const chosen = req.params.id;
 
+    for (let i = 0; i < notes.length; i++) {
+      if (chosen == notes[i].id) {
+        return res.json(notes[i]);
+      }
+    }
+    console.log("note not founded");
+    return res.json(false);
+  });
 
-// API router requests handle
-app.get("/api/notes", function (req, res) {
-  console.log("api notes loaded");
-  res.json(notes);
-});
+  // add new note to db.json
+  app.post("/api/notes", function (req, res) {
+    const newNotes = req.body;
 
-app.post("/api/notes", function (req, res) {
-  const newNotes = req.body;
+    notes.push(newNotes);
 
-  console.log(newNotes);
+    newNotes.id = notes.indexOf(newNotes);
 
-  notes.push(newNotes);
+    writeToDB(notes);
 
-  res.json(newNotes);
-});
+    res.json(newNotes);
+  });
 
-app.delete("/api/notes/:id", function (req, res) {});
+  // delete selected note
+  app.delete("/api/notes/:id", function (req, res) {
+    const chosen = req.params.id;
 
-// listener
-app.listen(PORT, function () {
-  console.log(`App is listening at PORT ${PORT}`);
-});
+    for (let i = 0; i < notes.length; i++) {
+      if (chosen == notes[i].id) {
+        newNotes = notes.slice(i);
+        writeToDB(newNotes);
+        return res.json(notes);
+      }
+    }
+    console.log("note not founded");
+    return res.json(false);
+  });
+}
+
+function writeToDB(data) {
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(outputPath, JSON.stringify(data));
+}
+
+function init() {
+  //middleware
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  app.use(express.json());
+
+  //router
+  htmlRoute();
+  apiRoute();
+
+  // listener
+  app.listen(PORT, function () {
+    console.log(`App is listening at PORT ${PORT}`);
+  });
+}
+
+init();
